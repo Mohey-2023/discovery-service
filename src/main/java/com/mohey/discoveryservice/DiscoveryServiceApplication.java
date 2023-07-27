09575c2d79fc28449fbd636971890f59f7add9ec
yxalaG <73926352+yxalaG@users.noreply.github.com>
2023-07-27 14:18:16 +0900

    🎨
    cicid test

--- src/main/java/com/mohey/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/com/mohey/discoveryservice/DiscoveryServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//잠시만 테스트 할게용..
+//잠시만 테스트 할게용..11
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
